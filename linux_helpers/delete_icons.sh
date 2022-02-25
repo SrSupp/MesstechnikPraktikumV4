a731c738f4713f9e8948a500b627bcdb54c9988f
Sven Suppelt <SrSupp@googlemail.com>
2022-02-25 19:58:09 +0100

    Der reale Roboterarm wird mit einem anderen PC verbunden. Die vorbereitungen werden mit diesem Commit getätigt

---
+++ linux_helpers/delete_icons.sh
@@ -0,0 +1,9 @@
+echo "Hiermit werden die Verküpfungen zum Starten von Helene entfernt"
+rm ~/.local/share/icons/messtechnik_praktikum_sim.png
+rm ~/.local/share/icons/messtechnik_praktikum_real_robot.png
+
+rm ~/.local/share/applications/messtechnik_praktikum_sim.desktop
+rm ~/.local/share/applications/messtechnik_praktikum_real_robot.desktop
+
+
+echo "Fertig - Verknüpfungen sind weg :("
--- linux_helpers/install_icons.sh
+++ linux_helpers/install_icons.sh
@@ -1,13 +1,11 @@
-echo "Hiermit werden die Verküpfungen zum Starten von Helene hinzugefügt"
+echo "Hiermit werden die Verküpfungen zum Starten von Helene hinzugefügt. Das hier ist allerdings alles für das Messtechnik Praktikum gedacht und ist mega spezifisch"
 mkdir -p ~/.local/share/icons
 mkdir -p ~/.local/share/applications
 
 cp messtechnik_praktikum_sim.png ~/.local/share/icons/messtechnik_praktikum_sim.png
 cp messtechnik_praktikum_real_robot.png ~/.local/share/icons/messtechnik_praktikum_real_robot.png
-cp Student.png ~/.local/share/icons/Student.png
 
 cp messtechnik_praktikum_sim.desktop ~/.local/share/applications/messtechnik_praktikum_sim.desktop
 cp messtechnik_praktikum_real_robot.desktop ~/.local/share/applications/messtechnik_praktikum_real_robot.desktop
-cp student.desktop ~/.local/share/applications/student.desktop
 
 echo "Fertig :)"
