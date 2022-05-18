d05b3c88d4dc7eb90757fb83e22751729ca329c7
Sven Suppelt <53657512+SrSupp@users.noreply.github.com>
2022-05-18 14:05:22 +0200

    Update Vorbereitungsaufgabe.py

--- studentworkspace/Vorbereitungsaufgabe.py
+++ studentworkspace/Vorbereitungsaufgabe.py
@@ -10,7 +10,7 @@ def sum_frames(a, b):
     return (ausgabe)
 
 pi = 3.14159265359
-Test_Koordinate = (0.2,0.05,0.3,*(pi,-pi/2,0))
-Add_Rel = (0,0.1,-0.15,*(0,0,0))
-Test_Koordinate = sum_frames(Test_Koordinate,Add_Rel)
-print(Test_Koordinate)
+Frame_Start = (0.2,0.05,0.3,*(pi,-pi/2,0))
+Frame_Offset = (0,0.1,-0.15,*(0,0,0))
+Frame_Ziel = sum_frames(Frame_Start,Frame_Offset)
+print(Frame_Ziel)
