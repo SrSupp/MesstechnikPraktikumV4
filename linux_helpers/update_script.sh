9e4cda768c1ca79684d9f44b99645aa2f3c34e3d
Sven Suppelt <suppelt@ad.must.e-technik.tu-darmstadt.de>
2022-07-06 16:57:36 +0200

    External Update Script

--- linux_helpers/update_script.sh
+++ linux_helpers/update_script.sh
@@ -1,13 +1,11 @@
-echo "Das hier ist das UpdateSkript. Hier werden alle Temporären Daten gelöscht und das Hintergrundbild gesetzt. Version: 06.07.2022"
+echo "Das hier ist das Update-Skript. Hier werden alle Temporären Daten gelöscht und das Hintergrundbild gesetzt. Version: 06.07.2022"
 
 rm -rf ~/.mozilla
 cd ~/ros_ws/src/MesstechnikPraktikumV6/linux_helpers/
 ./install_icons.sh
 cd ~/Downloads/; rm -rf * 
 
-cp ~/ros_ws/src/MesstechnikPraktikumV6/hardware/helene_background.png /usr/share/backgrounds/helene_background.png
-gsettings set org.gnome.desktop.background picture-uri
-usr/share/backgrounds/helene_background.png
 
+gsettings get org.gnome.desktop.background picture-uri'file:///home/pi/ros_ws/src/MesstechnikPraktikumV6/hardware/helene_background.png'
 
-echo "Fertig :)"
+echo "Fertig :), Viel Spaß im Praktikum!"
