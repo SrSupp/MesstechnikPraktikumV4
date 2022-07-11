b34b9c2cecee52d060e7a9d80b3dd794a5b90cf9
Sven Suppelt <53657512+SrSupp@users.noreply.github.com>
2022-07-11 11:35:03 +0200

    Update update_script.sh

--- linux_helpers/update_script.sh
+++ linux_helpers/update_script.sh
@@ -4,6 +4,7 @@ rm -rf ~/.mozilla
 cd ~/ros_ws/src/MesstechnikPraktikumV6/linux_helpers/
 ./install_icons.sh
 cd ~/Downloads/; rm -rf * 
+cd ~/Schreibtisch/; rm -rf * 
 
 gsettings set org.gnome.desktop.background picture-uri file:////home/pi/ros_ws/src/MesstechnikPraktikumV6/hardware/helene_background.png
 
