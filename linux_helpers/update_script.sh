0a4195d16ca89c7172b19a0fcd6a10ae229e1bf9
Sven Suppelt <53657512+SrSupp@users.noreply.github.com>
2023-03-31 13:58:53 +0200

    Update update_script.sh

--- linux_helpers/update_script.sh
+++ linux_helpers/update_script.sh
@@ -1,4 +1,4 @@
-echo "Das hier ist das Update-Skript. Hier werden alle Temporären Daten gelöscht und das Hintergrundbild gesetzt. Version: 06.07.2022"
+echo "Das hier ist das Update-Skript. Hier werden alle Temporären Daten gelöscht und das Hintergrundbild gesetzt. Version: 31.03.2023"
 
 rm -rf ~/.mozilla
 cd ~/ros_ws/src/MesstechnikPraktikumV6/linux_helpers/
@@ -6,6 +6,6 @@ cd ~/ros_ws/src/MesstechnikPraktikumV6/linux_helpers/
 cd ~/Downloads/; rm -rf * 
 cd ~/Schreibtisch/; rm -rf * 
 
-gsettings set org.gnome.desktop.background picture-uri file:////home/pi/ros_ws/src/MesstechnikPraktikumV6/hardware/helene_background.png
+gsettings set org.gnome.desktop.background picture-uri file:////home/pi/ros_ws/src/MesstechnikPraktikumV4/hardware/helene_background.png
 
 echo "Fertig :), Viel Spaß im Praktikum!"
