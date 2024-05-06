ba5686d4fb0eb8e14d5518fc63dbb64659db489a
Sven Suppelt <53657512+SrSupp@users.noreply.github.com>
2024-05-06 15:57:34 +0200

    Update update_script.sh
    Hintergrundbild weg

--- linux_helpers/update_script.sh
+++ linux_helpers/update_script.sh
@@ -1,4 +1,4 @@
-echo "Das hier ist das Update-Skript. Hier werden alle Temporären Daten gelöscht und das Hintergrundbild gesetzt. Version: 09.06.2023"
+echo "Das hier ist das Update-Skript. Hier werden alle Temporären Daten gelöscht und das Hintergrundbild gesetzt. Version: 06.05.2024"
 
 rm -rf ~/.mozilla
 cd ~/ros_ws/src/MesstechnikPraktikumV6/linux_helpers/
@@ -6,6 +6,7 @@ cd ~/ros_ws/src/MesstechnikPraktikumV6/linux_helpers/
 cd ~/Downloads/; rm -rf * 
 cd ~/Schreibtisch/; rm -rf * 
 
-gsettings set org.gnome.desktop.background picture-uri file:////home/pi/ros_ws/src/MesstechnikPraktikumV4/hardware/helene_background_eval.png
+#gsettings set org.gnome.desktop.background picture-uri file:////home/pi/ros_ws/src/MesstechnikPraktikumV4/hardware/helene_background_eval.png
+gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/warty-final-ubuntu.png
 
 echo "Fertig :), Viel Spaß im Praktikum!"
